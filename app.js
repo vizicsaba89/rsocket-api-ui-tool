7b53b867a2e3986e1f215d749078a573a8033931
vizicsaba <vizi.csaba@simplesoft.hu>
2020-03-02 22:10:36 +0100

    :sparkles: electron support

---
+++ app.js
@@ -0,0 +1,45 @@
+const { app, BrowserWindow } = require('electron')
+const url = require("url")
+const path = require("path")
+
+let appWindow
+
+function initWindow() {
+  appWindow = new BrowserWindow({
+    width: 1000,
+    height: 800,
+    webPreferences: {
+      nodeIntegration: true
+    }
+  })
+
+  appWindow.loadURL(
+    url.format({
+      pathname: path.join(__dirname, `/dist/spring-rsocket-client/index.html`),
+      protocol: "file:",
+      slashes: true
+    })
+  );
+
+  appWindow.webContents.openDevTools()
+
+  appWindow.on('closed', function () {
+    appWindow = null
+  })
+}
+
+app.on('ready', initWindow)
+
+app.on('window-all-closed', function () {
+
+  // On macOS specific close process
+  if (process.platform !== 'darwin') {
+    app.quit()
+  }
+})
+
+app.on('activate', function () {
+  if (win === null) {
+    initWindow()
+  }
+})
